bc3c920e8c70d942bfb506b7e7955451fec047f2
PedroFJ16 <1912011f@umich.mx>
2026-03-10 19:54:15 -0600

    insertar al inicio o al final de la lista

---
+++ listas.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Nodo{
+    int dato;
+    struct Nodo *siguiente;
+};
+
+struct Nodo* crearCabecera(){
+    struct Nodo *cabecera = malloc(sizeof(struct Nodo));
+    cabecera->siguiente = NULL;
+    return cabecera;
+}
+
+struct Nodo* crearNodo(int n){
+    struct Nodo *nuevo = malloc(sizeof(struct Nodo));
+    nuevo->dato = n;
+    nuevo->siguiente = NULL;
+    return nuevo;
+}
+
+void insertarInicio(struct Nodo *cabecera, struct Nodo *nuevo){
+    nuevo->siguiente = cabecera->siguiente;
+    cabecera->siguiente = nuevo;
+}
+void insertarFinal(struct Nodo *cabecera, struct Nodo *nuevo){
+    struct Nodo *aux = cabecera;
+
+    while(aux->siguiente != NULL){
+        aux = aux->siguiente;
+    }
+
+    aux->siguiente = nuevo;
+}
+
+void imprimirLista(struct Nodo *cabecera){
+    struct Nodo *aux = cabecera->siguiente;
+    while(aux != NULL){
+        printf("%d ", aux->dato);
+        aux = aux->siguiente;
+    }
+    printf("\n");
+}
+
+int main(){
+
+    struct Nodo *lista = crearCabecera();
+
+    int n, opcion;
+
+    while(1){
+
+        printf("Ingrese numero (-1 para salir): ");
+        scanf("%d",&n);
+
+        if(n == -1){
+            break;
+        }
+
+        printf("Inicio=1 Final=0: ");
+        scanf("%d",&opcion);
+
+        struct Nodo *nuevo = crearNodo(n);
+
+        if(opcion == 1){
+            insertarInicio(lista, nuevo);
+        }else{
+            insertarFinal(lista, nuevo);
+        }
+
+        printf("Lista actual: ");
+        imprimirLista(lista);
+    }
+
+    return 0;
+}
