5981ec4a345eca5960d717128f65c83de7504ad2
Gerardo <1911875k@umich.mx>
2026-06-13 23:18:55 -0600

    Faltaba impresion de uso de de RAM y disco

--- dispatch.c
+++ dispatch.c
@@ -125,7 +125,7 @@ struct TablaMarcos *sacarSuspendidos(struct Nodo *suspendidos, struct Nodo *list
             } else {
                 manecilla_reloj = algoritmoReloj(manecilla_reloj, listos, ejecutando, suspendidos, tmm, RAM);
                 marco_ram_nuevo = cargarARAM(aux_s->PID, aux_s->GID, pag_actual, bin, manecilla_reloj, listos, ejecutando, suspendidos, tms, tmm, RAM);
-                
+                porcentajeDiscoRAM(tmm, tms);
                 actualizaTMP(aux_s, tms); 
                 if (marco_ram_nuevo != -1) {
                     aux_s->tmp[pag_actual].num_marco_ram = marco_ram_nuevo;
